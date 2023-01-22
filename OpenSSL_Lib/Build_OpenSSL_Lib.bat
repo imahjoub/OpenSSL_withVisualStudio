ec865deafa9d3ed9004f01c48bbf5aa36767ca0a
Iliass Mahjoub <mahjoub.iliass@gmail.com>
2023-01-22 22:01:51 +0100

    add build parameters to Build_OpenSSL_Lib.bat

--- OpenSSL_Lib/Build_OpenSSL_Lib.bat
+++ OpenSSL_Lib/Build_OpenSSL_Lib.bat
@@ -7,4 +7,26 @@
 
 @echo off
 
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+
 echo "Run Build_OpenSSL_Lib.bat ..."
+
+set Configuration=%1
+set Platform=%2
+echo %Configuration%
+echo %Platform%
+
+echo "Setting up Build environement ..."
+::cpan -i Text::Template
+::cpan -i Test::More
+
+nmake /? 
+
+
+echo "Build %Configuration% %Platform%"
+::perl Configure VC-WIN64A --prefix=%cd%_Bin\Lib\%Configuration%\%Platform% --openssldir=%cd% no-shared
+
+perl Configure VC-WIN64A --prefix=%cd%\OpenSSL_Lib\_Bin\Lib\%Configuration%_%Platform% --openssldir=%cd%\OpenSSL_Lib no-shared
+
+nmake install_sw
+
