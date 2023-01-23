bdd424fa511b7ac9967e0bc2f95455d9fcaa6d06
Iliass Mahjoub <mahjoub.iliass@gmail.com>
2023-01-23 23:12:00 +0100

    add file header to test files

--- OpenSSL_Test/OpenSSL_Test.cpp
+++ OpenSSL_Test/OpenSSL_Test.cpp
@@ -1,3 +1,10 @@
+// -----------------------------------------------------
+//  Copyright Iliass Mahjoub 2023.                     
+//  Distributed under the Boost Software License,      
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)   
+// -----------------------------------------------------
+
 #include <array>
 #include <iostream>
 
--- OpenSSL_Test/Sign_and_Verify_Message _with_Openssl_ECDSA.cpp
+++ OpenSSL_Test/Sign_and_Verify_Message _with_Openssl_ECDSA.cpp
@@ -1,4 +1,10 @@
-// filename: main.cpp
+// -----------------------------------------------------
+//  Copyright Iliass Mahjoub 2023.                     
+//  Distributed under the Boost Software License,      
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)   
+// -----------------------------------------------------
+
 #include <string.h>
 #include "openssl/ec.h"
 #include "openssl/ecdsa.h"
