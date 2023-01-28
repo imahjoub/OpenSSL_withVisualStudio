62a67cce461dec34f425eca788706f962b800746
Iliass Mahjoub <mahjoub.iliass@gmail.com>
2023-01-28 17:43:26 +0100

    update OpenSSL_Test_codeql.yml

--- OpenSSL_Lib/Build_OpenSSL_Lib.bat
+++ OpenSSL_Lib/Build_OpenSSL_Lib.bat
@@ -7,22 +7,28 @@
 
 @echo off
 
-echo "Run Build_OpenSSL_Lib.bat %1 %2"
+::echo "Run Build_OpenSSL_Lib.bat %1 %2"
+
+echo "print perl version"
+perl -v
+
+echo "print perl help"
+perl --help
 
 :: TBD make this relativ
 ::call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
-set Configuration=%1
-set Platform=%2
-set OpenSSL_Dir=%cd%
-
-if NOT x%Configuration:Debug=%==x%Configuration% (
-echo "Build %Configuration% %Platform%"
-perl Configure VC-WIN64A --prefix=%OpenSSL_Dir%\_Bin\%Configuration%_%Platform% --openssldir=%OpenSSL_Dir% no-shared
-nmake install_sw
-
-) else (
-echo "Build %Configuration% %Platform%"
-perl Configure VC-WIN64A --prefix=%OpenSSL_Dir%\_Bin\%Configuration%_%Platform% --openssldir=%OpenSSL_Dir% no-shared
-nmake install_sw
-)
+::set Configuration=%1
+::set Platform=%2
+::set OpenSSL_Dir=%cd%
+::
+::if NOT x%Configuration:Debug=%==x%Configuration% (
+::echo "Build %Configuration% %Platform%"
+::perl Configure VC-WIN64A --prefix=%OpenSSL_Dir%\_Bin\%Configuration%_%Platform% --openssldir=%OpenSSL_Dir% no-shared
+::nmake install_sw
+::
+::) else (
+::echo "Build %Configuration% %Platform%"
+::perl Configure VC-WIN64A --prefix=%OpenSSL_Dir%\_Bin\%Configuration%_%Platform% --openssldir=%OpenSSL_Dir% no-shared
+::nmake install_sw
+::)
