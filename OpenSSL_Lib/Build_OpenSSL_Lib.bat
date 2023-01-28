59c39d558a278f33cb565036548ac48ca757588a
Iliass Mahjoub <mahjoub.iliass@gmail.com>
2023-01-28 17:27:36 +0100

    try to build lib after getting perl available

--- OpenSSL_Lib/Build_OpenSSL_Lib.bat
+++ OpenSSL_Lib/Build_OpenSSL_Lib.bat
@@ -9,22 +9,20 @@
 
 echo "Run Build_OpenSSL_Lib.bat %1 %2"
 
-perl -v
-
 :: TBD make this relativ
 ::call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
-::set Configuration=%1
-::set Platform=%2
-::set OpenSSL_Dir=%cd%
-::
-::if NOT x%Configuration:Debug=%==x%Configuration% (
-::echo "Build %Configuration% %Platform%"
-::perl Configure VC-WIN64A --prefix=%OpenSSL_Dir%\_Bin\%Configuration%_%Platform% --openssldir=%OpenSSL_Dir% no-shared
-::nmake install_sw
-::
-::) else (
-::echo "Build %Configuration% %Platform%"
-::perl Configure VC-WIN64A --prefix=%OpenSSL_Dir%\_Bin\%Configuration%_%Platform% --openssldir=%OpenSSL_Dir% no-shared
-::nmake install_sw
-::)
+set Configuration=%1
+set Platform=%2
+set OpenSSL_Dir=%cd%
+
+if NOT x%Configuration:Debug=%==x%Configuration% (
+echo "Build %Configuration% %Platform%"
+perl Configure VC-WIN64A --prefix=%OpenSSL_Dir%\_Bin\%Configuration%_%Platform% --openssldir=%OpenSSL_Dir% no-shared
+nmake install_sw
+
+) else (
+echo "Build %Configuration% %Platform%"
+perl Configure VC-WIN64A --prefix=%OpenSSL_Dir%\_Bin\%Configuration%_%Platform% --openssldir=%OpenSSL_Dir% no-shared
+nmake install_sw
+)
